afae2b218b6c0b6a73232321f1c6ed7ab9b04399
Lumbani-themaker <lu.sowoya@gmail.com>
2021-12-09 13:40:57 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("menu").classList.toggle('active');
+  }
+  function closeNav() {
+    document.getElementById("menu").classList.toggle('active');
+  }
+
+  function Slide() {
+    document.getElementById("center-part").classList.toggle('slide');
+  }
